9070918455f3a278fa3eb1953af2b754a3d73a52
Sagar S <ganesha4us1@gmail.com>
2023-12-14 21:18:29 -0800

    Finished!! Pretty simple!

--- Heart_Failure.py
+++ Heart_Failure.py
@@ -1,65 +1,58 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
+from sklearn.impute import SimpleImputer
 from sklearn.linear_model import LogisticRegression
+from ucimlrepo import fetch_ucirepo
 
-# # Sample data for testing
-# data = {
-#     'Age': [30, 45, 55, 65, 25, 40, 50, 60, 35, 42],
-#     'Sex': [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],  # 1 for male, 0 for female
-#     'BMI': [22, 26, 28, 31, 24, 28, 25, 30, 27, 29],
-#     'BPM': [70, 80, 75, 85, 72, 72, 78, 68, 85, 76],
-#     'HeartFailure': [0, 1, 1, 0, 0, 0, 1, 0, 1, 0]  # 1 for potential heart failure, 0 for no potential heart failure
-# }
+# fetch dataset
+heart_disease = fetch_ucirepo(id=45)
 
-# df = pd.DataFrame(data)
+# data (as pandas dataframes)
+X = heart_disease.data.features
+y = heart_disease.data.targets
 
-# # Separate features and target variable
-# X = df[['Age', 'Sex', 'BMI', 'BPM']]
-# y = df['HeartFailure']
-
-from ucimlrepo import fetch_ucirepo 
-  
-# fetch dataset 
-heart_disease = fetch_ucirepo(id=45) 
-  
-# data (as pandas dataframes) 
-X = heart_disease.data.features 
-y = heart_disease.data.targets 
-
+# Handle NaN values with SimpleImputer
+imputer = SimpleImputer(strategy='mean')
+X_imputed = imputer.fit_transform(X)
 
 # Standardize the features
 scaler = StandardScaler()
-X_scaled = scaler.fit_transform(X)
+X_scaled = scaler.fit_transform(X_imputed)
 
 # Train a logistic regression model
 model = LogisticRegression()
-#model.fit(X, y)
-
+model.fit(X_scaled, y)
 
 def predict(new_data):
-# Standardize the new data
-    new_data_scaled = scaler.transform(new_data)
+    # Create a DataFrame with the same column names as the training data
+    new_data_with_columns = pd.DataFrame(new_data, columns=X.columns)
+
+    # Handle NaN values in new data
+    new_data_imputed = imputer.transform(new_data_with_columns)
+    
+    # Standardize the new data
+    new_data_scaled = scaler.transform(new_data_imputed)
 
     # Make predictions
     predictions = model.predict(new_data_scaled)
+    
     # Display predictions
     for i, prediction in enumerate(predictions):
-        result = 'You have a potential Heart Failure. Please seek medical help immediately.' if prediction == 1 else 'Yay. You have no potential Heart Failure'
-        print(f"Test case {i + 1}: {result}")
+        result = 'You have a potential Heart Failure. Please seek medical help immediately.' if prediction == 1 else 'Yay. You have no potential for Heart Failure'
+        print(result)
 
 def main():
-    print("Enter: <age> <sex> <Ca> <Cholesterol> <Resting BPS> <Max Heart Rate Achieved> :")
+    print("Enter: <Age> <Sex> <Smoke (1 = y, 0 = n)> <Cholesterol> <Resting BPS> <Max Heart Rate Achieved> <Diabetes (1 = y, 0 = n):")
     new_data = pd.DataFrame({
-        'age': [int(input())],
-        'sex': [int(input())],
-        'ca': [float(input())],
+        'age': [float(input())],
+        'sex': [float(input())],
+        'smoke': [float(input())],
         'chol': [float(input())],
         'trestbps': [float(input())],
-        'thalach': [int(input())]
+        'thalach': [float(input())],
+        'dm': [float(input())]
     })
     predict(new_data)
 
 if __name__ == "__main__":
     main()
-
-    
